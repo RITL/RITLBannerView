a8c420ce2b8b7547e3e699e19f3be540ac04a1e7
774655048@qq.com <774655048@qq.com>
2016-07-21 10:40:39 +0800

    Initial Commit

---
+++ CollectionBannerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionBannerView
+//
+//  Created by YueWen on 16/7/21.
+//  Copyright © 2016年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
